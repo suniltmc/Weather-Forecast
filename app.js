534ab6b983361b8f0122e22df4c41ce1fd583542
suniltmc <suniltmc@gmail.com>
2015-12-29 15:11:44 -0500

    Addind and setting up to get the data from weather API

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var WeatherForecast = angular.module('WeatherForecast', ['ngRoute', 'ngResource']);
 
+//Routing
 WeatherForecast.config(function($routeProvider){
 	$routeProvider
 		.when('/', {
@@ -13,10 +14,12 @@ WeatherForecast.config(function($routeProvider){
 		});
 });
 
+//Services
 WeatherForecast.service('cityService', function(){
-	this.city ="Hyderabad";
+	this.city ="New York";
 })
 
+//Controllers
 WeatherForecast.controller('homeController', ['$scope', 'cityService', function($scope, cityService){
 	$scope.city = cityService.city;
 
@@ -26,7 +29,14 @@ WeatherForecast.controller('homeController', ['$scope', 'cityService', function(
 	
 }]);
 
-WeatherForecast.controller('forecastController', ['$scope', 'cityService', function($scope, cityService){
+WeatherForecast.controller('forecastController', ['$scope', '$resource', 'cityService', function($scope, $resource, cityService){
 	$scope.city = cityService.city;
+	$scope.weatherAPPID = '35eceb4011ce1121020cb39012dba9df'
+
+	$scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", {callback:"JSON_CALLBACK"}, {get:{method: "JSONP"}});
+
+	$scope.weatherResult = $scope.weatherAPI.get({ q:$scope.city, cnt: 2, APPID : $scope.weatherAPPID })
+
+	console.log($scope.weatherResult);
 }]);
 
